23f3c686635eae04a1fecdfb931c5645bda6e11f
ryancwilson <35963118+ryancwilson@users.noreply.github.com>
2018-03-15 10:52:44 +0000

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Thanks!</title>
+  <meta name="description" content="Thank you page">
+  <meta name="author" content="SitePoint">
+  </head>
+  <body>
+
+
+<?php
+
+    echo "Thanks for the enquiry!"; 
+
+?>
+
+
+</body>
+</html>
