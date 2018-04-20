f3d94bef0e05fa2d12f849fba41bc946869727b8
Ryan Wilson <35963118+ryancwilson@users.noreply.github.com>
2018-04-20 14:38:45 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,12 +15,7 @@ jQuery(function($) {
 		});
 						
 	});	
-	$('a').click(function(){
-	    $('html, body').animate({
-		scrollTop: $( $(this).attr('href') ).offset().top
-	    }, 500);
-	    return false;
-	});
+	
 	
 
 	//Initiat WOW JS
@@ -64,3 +59,24 @@ jQuery(function($) {
 	
 	
 });
+// Wrap every letter in a span
+$('.ml6 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
