a43b597af79b24af3efaf01d52ad89149f355b03
Ryan Wilson <35963118+ryancwilson@users.noreply.github.com>
2018-04-20 14:36:17 +0100

    Update mailer.php

--- forms/mailer.php
+++ forms/mailer.php
@@ -1,18 +1,26 @@
 <?php
     ob_start();
+
+    function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+
     $to = "ryancwilson@live.co.uk";
-    $subject = "Support requested by ".$_POST['name'];
-    $name_field = $_POST['name'];
-	$phone_field = $_POST['phone'];
-    $email_field = $_POST['email'];
-    $message = $_POST['message'];
-    
-    $headers = 'From: '.$_POST['email'].'' . "\r\n" .
-       'Reply-To: '.$_POST['email'].'' . "\r\n" .
+    $subject = "Support requested by ".test_input($_POST['name']);
+    $name_field = test_input($_POST['name']);
+	  $phone_field = test_input($_POST['phone']);
+    $email_field = test_input($_POST['email']);
+    $message = test_input($_POST['message']);
+
+    $headers = 'From: '.test_input($_POST['email']).'' . "\r\n" .
+       'Reply-To: '.test_input($_POST['email']).'' . "\r\n" .
        'X-Mailer: PHP/' . phpversion();
-    
+
     $body = 'Name: ' . $name_field . "\r\n" . 'Phone Number: ' . $phone_field . "\r\n" . 'Message: ' . $message;
-    
+
     @mail($to, $subject, $body, $headers );
     header( 'Location:thankyou.php' ) ; //replace with landing page.
     exit();
